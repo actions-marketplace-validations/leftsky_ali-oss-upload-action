7b6b8c536133cd12412d46dfc1ae42458b47f8c8
leftsky <leftsky@vip.qq.com>
2024-12-15 17:04:18 +0800

    feature

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-import {getInput} from "@actions/core";
-import OSS from "ali-oss";
-import fs from "fs";
+import {getInput} from '@actions/core'
+import OSS from 'ali-oss'
+import fs from 'fs'
 
 const config = {
   accessKeyId: getInput('accessKeyId'),
   accessKeySecret: getInput('accessKeySecret'),
   region: getInput('region'),
-  bucket: getInput('bucket'),
+  bucket: getInput('bucket')
 }
 
-if (!config.accessKeyId || !config.accessKeySecret || !config.region || !config.bucket) {
-  throw new Error('请配置accessKeyId, accessKeySecret, region, bucket');
+if (
+  !config.accessKeyId ||
+  !config.accessKeySecret ||
+  !config.region ||
+  !config.bucket
+) {
+  throw new Error('请配置accessKeyId, accessKeySecret, region, bucket')
 }
 
-const dir = getInput('dir');
-const targetOssDir = getInput('targetOssDir');
+const dir = getInput('dir')
+const targetOssDir = getInput('targetOssDir')
 if (!dir || !targetOssDir) {
-  throw new Error('请配置上传目录和目标目录');
+  throw new Error('请配置上传目录和目标目录')
 }
 
 const client = new OSS({
@@ -24,23 +29,21 @@ const client = new OSS({
   // authorizationV4: true,
 })
 
-
 const uploadFiles = async (dir: string, targetDir: string): Promise<void> => {
   try {
-    const files = fs.readdirSync(dir);
+    const files = fs.readdirSync(dir)
     for (const file of files) {
-      const filePath = dir + file;
-      const stat = fs.statSync(filePath);
+      const filePath = dir + file
+      const stat = fs.statSync(filePath)
       if (stat.isDirectory()) {
-        await uploadFiles(filePath + '/', targetDir + file + '/');
+        await uploadFiles(filePath + '/', targetDir + file + '/')
       } else {
-        await client.put(targetDir + file, filePath);
+        await client.put(targetDir + file, filePath)
       }
     }
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
-};
+}
 
 uploadFiles(dir, targetOssDir)
-
